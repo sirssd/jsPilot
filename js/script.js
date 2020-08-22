f06144f12189fd2ea10c630a053bc02f888eba5d
Sergey Durov <sergssd@yandex.com>
2020-08-22 20:02:00 +0500

    'Homework'

--- js/script.js
+++ js/script.js
@@ -16,28 +16,48 @@ for (let i = 0; i < 2; i++) {
     let question1 = prompt('Введите обязательную статью расходов в этом месяце');
     let question2 = prompt('Во сколько обойдется?');
 
-    if((typeof question1) === 'string' && (typeof question1) != null && (typeof question2) != null
-        && question1 != '' && question2 != '' && question1.length < 50) {
-            console.log('Успех');
+    if ((typeof question1) === 'string' && (typeof question1) != null && (typeof question2) != null &&
+        question1 != '' && question2 != '' && question1.length < 50) {
+        console.log('Успех');
         appData.expenses[question1] = question2;
     } else {
         i--;
     }
 }
 
-appData.moneyPerDay = appData.budget / 30;
+function detectDayBudget() {
+    return appData.moneyPerDay = appData.budget / 30;
+}
+
+
+detectDayBudget();
 
 alert('Ежедневный бюджет = ' + appData.moneyPerDay);
 
-if(appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка.');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка.');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка.');
-} else {
-    console.log('Произошла Ошибка!');
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log('Минимальный уровень достатка.');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Средний уровень достатка.');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Высокий уровень достатка.');
+    } else {
+        console.log('Произошла Ошибка!');
+    }
 }
 
+function chooseOptExpenses () {
+    for(let i = 0; i < 3; i++) {
+        let question = prompt('Статья необязательных расходов?');
+        appData.optionalExpenses[i + 1] = question;
+    }
+}
+
+
+detectLevel();
+chooseOptExpenses();
+
+console.log(appData.optionalExpenses);
 console.log(appData.budget);
 console.log(appData.expenses);
+console.log(appData);
